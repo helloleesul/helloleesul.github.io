592fe32e177fca59b2a7282e6048916f2549f7cb
LEE SUL <suerish.e@gmail.com>
2024-01-07 23:54:09 +0900

    Doc: gh-pages 배포 준비

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: `leesulog`,
     description: `개발 공부를 하며 배우고 느낀 것들을 기록합니다.`,
     author: `leesul`,
-    siteUrl: 'https://helloleesul.github.io',
+    siteUrl: 'https://helloleesul.github.io/',
   },
   plugins: [
     {
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://helloleesul.github.io',
+        siteUrl: 'https://helloleesul.github.io/',
         stripQueryString: true,
       },
     },
