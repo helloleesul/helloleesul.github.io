ea8903ce2ac98a3c27c1f49b48fe191e6b1972f3
LEE SUL <suerish.e@gmail.com>
2024-03-25 15:57:03 +0900

    Chore: About 사용 안함

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -9,7 +9,7 @@ import {
   LinkGroup,
   Description,
 } from './styled';
-import { Link } from 'gatsby';
+// import { Link } from 'gatsby';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
@@ -26,14 +26,14 @@ export default function Profile({ profileImage }: ProfileProps) {
       <Description>
         <Title>leesul, 이슬</Title>
         <SubTitle>
-          web frontend developer
+          frontend developer
           <br />
           스스로 올바른 선택을 할 수 있는 사람이 될 것
         </SubTitle>
         <LinkGroup>
-          <li>
+          {/* <li>
             <Link to="/about">About</Link>
-          </li>
+          </li> */}
           {/* 이름, 소개, 소셜링크 따로 받아와서 노출시키기 */}
           <li>
             <a href="https://github.com/helloleesul" target="_blank">
